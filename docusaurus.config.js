5d164a4b61c114026f8917e7a6e3cec107ca7b48
MingMimi96 <mingmimi96@gmail.com>
2025-11-22 10:53:43 +0800

    Personalize blog title and tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Makier Journey',
+  title: 'My Maker Journey',
   tagline: 'Buidling, Learning, Creating',
   favicon: 'img/favicon.ico',
 
